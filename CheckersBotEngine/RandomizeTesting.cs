c7b79d2246d104d7a8676cdf0625cee6e960f509
Vitaliy Novak <vitaliynovak555@gmail.com>
2023-09-17 18:49:38 +0300

    RandomizeTesting | Executor of random actions for demo\testing

---
+++ CheckersBotEngine/RandomizeTesting.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckersBotEngine
+{
+    public static class RandomizeTesting
+    {
+        static GameField GameField { get; set; }
+
+        public static void Run(GameField gameField)
+        {
+            // Print starting game field state
+            GameField = gameField;
+            Console.WriteLine(GameField);
+
+            // Simulate game by random steps
+            ActionsExecutor actionsExecutor = new ActionsExecutor(gameField);
+            for( int i = 0; i < 10; i++ )
+            {
+                var pos = FindChecker(i % 2 == 0);
+                var actions = ActionsGenerator.GetCheckerActions(pos, gameField);
+                if( actions.Count() == 0)
+                {
+                    i--;
+                    continue;
+                }
+
+                Random random = new Random();
+                var actionId =random.Next(0, actions.Count());
+                actionsExecutor.ExecuteAction(actions[actionId]);
+                Console.WriteLine("________________________________________________________");
+                Console.ReadKey();
+                Console.WriteLine(GameField);
+            }
+        }
+
+        public static FieldPosition FindChecker( bool findWhite )
+        {
+            Random random = new Random();
+            Checker checker = Checker.None;
+            FieldPosition position;
+            do
+            {
+                int x = random.Next(0, 8);
+                int y = random.Next(0, 8);
+                position = new FieldPosition(x, y);
+                checker = GameField.GetCheckerAtPosition(position);
+            } while (checker == Checker.None || checker.isWhite() != findWhite);
+            return position;
+        }
+    }
+}
